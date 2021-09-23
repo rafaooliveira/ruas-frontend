7a78acdafaa86a2e8cf60640ed9b4ee7c5aa9411
rafaooliveira <raafasantos1@gmail.com>
2021-09-22 21:49:36 -0300

    adding promoter and artist properties to user

--- src/pages/Cadastro.vue
+++ src/pages/Cadastro.vue
@@ -14,34 +14,6 @@ div.q-pa-xl
 
 <script>
 import User from "src/components/TiposDeUsuario/User/User.vue"
-//-  Deve ter uma tela de cadastro de promotor e não na tela de cadastro 
-//- Artistas.col-xl-3.col-lg-4.col-md-6.col-sm-6.col-xs-12(
-//- 	v-if="register.tipoUsuario === 'artista'"
-//- 	:vulgo="vulgo"
-//- 	:avaliacao="avaliacao"
-//- 	@avaliacaoUpdate="avaliacaoUpdate"
-//- 	@vulgoUpdate="vulgoUpdate"
-//- )
-//- Promotores.col-xl-3.col-lg-4.col-md-6.col-sm-6.col-xs-12(
-//- 	v-if="register.tipoUsuario === 'promotor'"
-//- 	:agencia="agencia"
-//- 	@agenciaUpdate="agenciaUpdate"
-//- )
-// import Artistas from "components/TiposDeUsuario/Artistas.vue"
-// import Promotores from "components/TiposDeUsuario/Promotores.vue"
-// components:{
-// 	Artistas,
-// 	Promotores
-// },
-// avaliacaoUpdate (avaliacao) {
-// 	this.avaliacao = avaliacao
-// },
-// vulgoUpdate (vulgo) {
-// 	this.vulgo = vulgo
-// },
-// agenciaUpdate (agencia) {
-// 	this.agencia = agencia
-// }
 
 export default {
 	name: "Cadastro",
